3d856b606c56c7eb96e3f5bdd7ad321bf6cb9458
magic oggy <ogon17@gmail.com>
2019-10-17 03:20:52 +0200

    display clients bug fix

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-let clients = [
-    {
-      "name": "Jan",
-      "surname": "Kowalski",
-      "balance": "1000"
+let clients = [{
+        "name": "Jan",
+        "surname": "Kowalski",
+        "balance": "1000"
     },
     {
-      "name": "Piotr",
-      "surname": "Nowak",
-      "balance": 5000
+        "name": "Piotr",
+        "surname": "Nowak",
+        "balance": 5000
     }
-  ];
+];
 
-for (client of clients){
-    console.log(client.name);
-    document.getElementById("clients").innerHTML = client.name +' '+ client.surname;
+
+
+for (client of clients) {
+    let newClient = document.createElement("li");
+    let setClient = document.createTextNode(client.name + ' '+ client.surname +'-'+ client.balance);
+    newClient.appendChild(setClient);
+    document.getElementById("clients").appendChild(newClient);
 }
-
