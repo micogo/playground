7296ff76e9dd22404c1e1b589510940ec8bac366
magic oggy <ogon17@gmail.com>
2019-10-17 17:56:23 +0200

    changes in js data

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let clients = [{
+var clients = [{
         "name": "Jan",
         "surname": "Kowalski",
         "balance": "1000"
@@ -7,6 +7,11 @@ let clients = [{
         "name": "Piotr",
         "surname": "Nowak",
         "balance": 5000
+    },
+    {
+        "name": "Beata",
+        "surname": "Rypło",
+        "balance": -10000
     }
 ];
 
@@ -14,7 +19,13 @@ let clients = [{
 
 for (client of clients) {
     let newClient = document.createElement("li");
-    let setClient = document.createTextNode(client.name + ' '+ client.surname +'-'+ client.balance);
+    let setClient = document.createTextNode(client.name + ' ' + client.surname + ': ' + client.balance);
     newClient.appendChild(setClient);
     document.getElementById("clients").appendChild(newClient);
 }
+
+clients.push({"name":"Janusz","surname":"KorwinDog","balance":"1000000"});
+console.log(clients);
+
+let bydło = clients.slice(2,3);
+console.log(bydło);
